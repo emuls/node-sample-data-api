ad514f01cb36d1f8d32913c6249c2e24c152230d
Rex Jolley <emulsifier@gmail.com>
2015-12-23 22:29:03 -0700

    adding ability to read data from disk

--- src/server.js
+++ src/server.js
@@ -1,12 +1,24 @@
-var data = {
-    "dogs" : [
-        "Labrador",
-        "Golden Retriever",
-        "Malteze"
-    ]
-};
-
+var fs = require('fs');
 var express = require('express');
+
+var dataFile = process.argv[2];
+var data;
+
+if(!dataFile){
+    dataFile = 'sample-data.json';
+}
+
+fs.readFile(dataFile, 'utf8', function(err, fileData) {
+    if (err){
+        console.log("Error loading data: " + err);
+        console.log("Setting data to empty map");
+        data = {};
+    }else {
+        console.log('Read Data File: ' + dataFile);
+        data = JSON.parse(fileData);
+    }
+});
+
 var app = express();
 
 app.use('/', express.static('www'));
@@ -20,5 +32,5 @@ var server = app.listen(3000, function () {
     var host = server.address().address;
     var port = server.address().port;
 
-    console.log('Sample Data Server listening at http://%s:%s', host, port);
+    console.log('Sample Data Server initialized with "' + dataFile + '" listening at http://%s:%s', host, port);
 });
