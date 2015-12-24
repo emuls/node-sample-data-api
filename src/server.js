219ee3396284483ab86f8059032d574bc7e911c7
Rex Jolley <emulsifier@gmail.com>
2015-12-23 22:07:29 -0700

    Updating package.json to depend on express, implementing server.js static content and index page

--- src/server.js
+++ src/server.js
@@ -1,1 +1,17 @@
 var data = {};
+
+var express = require('express');
+var app = express();
+
+app.use('/', express.static('www'));
+
+app.get('/hello', function (req, res) {
+    res.send('Hello!');
+});
+
+var server = app.listen(3000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Sample Data Server listening at http://%s:%s', host, port);
+});
